00a1b8b15bfa711b84a8399276130ccd506ab507
Mrutyunjay Dash <mdash555@gmail.com>
2021-07-26 01:50:09 +0530

    first commit

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { HeartTwoTone } from "@ant-design/icons";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Made with <HeartTwoTone twoToneColor="#eb2f96" /> By{" "}
+      <a
+        className="footer__link"
+        href="https://github.com/mihir0699/Video-Chat"
+        target="_blank"
+        style={{ color: "#fdfdfd" }}
+        rel="noreferrer"
+      >
+        Mrutyunjay Dash
+      </a>
+      <br />
+    </footer>
+  );
+};
+
+export default Footer;
